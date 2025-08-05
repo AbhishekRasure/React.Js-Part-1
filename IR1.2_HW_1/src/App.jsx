89463b55243b5140f4b247e339479da6f8cf141f
Abhishek Rasure <abhishekrasure264@gmail.com>
2025-08-05 19:32:59 +0530

    IR1.2_HW_1

---
+++ IR1.2_HW_1/src/App.jsx
@@ -0,0 +1,115 @@
+import { useState } from 'react'
+
+const BookInformation = () => {
+  const book = {
+
+    title: "1984",
+    author: "George Orwell",
+    genre: "Dystopian Fiction",
+    year: 1949
+  }
+  return (
+    <div>
+      <h1>Title: {book.title}</h1>
+      <p>Author : {book.author}</p>
+      <p>Genre : {book.genre}</p>
+      <p>Year : {book.year}</p>
+    </div>
+  )
+}
+
+const BlogDetails = () => {
+    const blog = {
+
+    title: "How to Start a Blog",
+    author: "Jane Smith",
+    date: "February 28, 2024",
+    likes: 150
+   }
+   
+   return (
+    <div>
+      <h1>Title : {blog.title}</h1>
+      <p>Author : {blog.author}</p>
+      <p>Date : {blog.date}</p>
+      <p>Likes : {blog.likes}</p>
+    </div>
+   )
+}
+
+const MovieProfile = () => {
+  const movieProfile = {
+
+    title: "The Shawshank Redemption",
+    director: "Frank Darabont",
+    rating: 9.3
+  }
+
+  return (
+    <div>
+      <h1>Title: {movieProfile.title}</h1>
+      <p>Director : {movieProfile.director}</p>
+      <p>Rating : {movieProfile.rating}</p>
+    </div>
+  )
+}
+
+const ProductDetails = () => {
+  const product = {
+
+    name: "Smartphone",
+    brand: "Samsung",
+    price: 799.99
+  }
+
+  return (
+    <div>
+      <p>Name : {product.name}</p>
+      <p>Brand : {product.brand}</p>
+      <p>Price : {product.price}</p>
+    </div>
+  )
+}
+
+const BookDetails = () => {
+  const book = {
+
+  title: "The Hitchhiker's Guide to the Galaxy",
+  author: {
+    name: "Douglas Adams",
+    nationality: "British"
+  },
+  publicationYear: 1979,
+  genres: ["Science Fiction", "Comedy"]
+
+  }
+
+  return (
+    <div>
+      <h1>{book.title}</h1>
+      <p>Author: {book.author.name} ({book.author.nationality})</p>
+      <p>Publication Year: {book.publicationYear}</p>
+      <p>Genres : {book.genres}</p>
+    </div>
+  )
+}
+
+function App() {
+  
+  return (
+    <>
+      <h1>Book Information</h1>
+      <BookInformation/>
+      <h1>Blog Details</h1>
+      <BlogDetails/>
+      <h1>Movie Profile</h1>
+      <MovieProfile/>
+      <h1>Product Details</h1>
+      <ProductDetails/>
+      <h1>Book Details</h1>
+      <BookDetails/>
+    </>
+  )
+}
+
+export default App
